1a1591de88e0a49189df4e00504baa0f23403a22
liamht <ljht93@hotmail.com>
2018-07-15 13:45:12 +0100

    Forgot to add new navigation methods to the interface

--- APIGirrafe.UI/Navigation/INavigationHelper.cs
+++ APIGirrafe.UI/Navigation/INavigationHelper.cs
@@ -1,14 +1,25 @@
 ﻿using APIGirrafe.UI.ViewModels;
+using System;
 using System.Windows.Controls;
 
 namespace APIGirrafe.UI.Navigation
 {
     public interface INavigationHelper
     {
+        [Obsolete("Consider passing the type of usercontrol and viewmodel as arguments so the navigation helper can create new instances of the objects itself")]
         void NavigateTo(UserControl page, BasePageViewModel vm);
 
+        void NavigateTo<TUserControl, TViewModel>() where TUserControl : UserControl where TViewModel : BasePageViewModel;
+
+        void NavigateTo<TUserControl, TViewModel>(Action<TViewModel> postNavigationAction) where TUserControl : UserControl where TViewModel : BasePageViewModel;
+
+        [Obsolete("Consider passing the type of usercontrol and viewmodel as arguments so the navigation helper can create new instances of the objects itself")]
         void ShowModal(UserControl content, BasePageViewModel vm);
-        
+
+        void ShowModal<TUserControl, TViewModel>() where TUserControl : UserControl where TViewModel : BasePageViewModel;
+
+        void ShowModal<TUserControl, TViewModel>(Action<TViewModel> postNavigationAction) where TUserControl : UserControl where TViewModel : BasePageViewModel;
+
         void DestroyModal();
 
         void RefreshMenu();
