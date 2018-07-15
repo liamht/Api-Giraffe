f0ee25d29d149d3c8d3ac67ce89def42f78fb1f5
liamht <ljht93@hotmail.com>
2018-07-15 22:09:13 +0100

    Added tests for header factory

---
+++ APIGiraffe.ApplicationServices.Test/Requests/Commands/AddNewHeader/Factory/HeaderFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using APIGiraffe.ApplicationServices.Requests.Commands.AddNewHeader.Factory;
+using APIGiraffe.Domain;
+using Xunit;
+
+namespace APIGiraffe.ApplicationServices.Test.Requests.Commands.AddNewHeader.Factory
+{
+    public class HeaderFactoryTests
+    {
+        private readonly HeaderFactory _subject;
+
+        public HeaderFactoryTests()
+        {
+            _subject = new HeaderFactory();
+        }
+
+        [Fact]
+        public void Create_CreatesNewHeader()
+        {
+            var result = _subject.Create("hello", "world");
+
+            Assert.IsType<Header>(result);
+        }
+
+        [Fact]
+        public void Create_SetsValues()
+        {
+            var result = _subject.Create("hello", "world");
+
+            Assert.Equal("hello", result.Name);
+            Assert.Equal("world", result.Value);
+        }
+
+        [Fact]
+        public void Create_WhenNameIsNull_SetsNullValueInReturnObject()
+        {
+            var result = _subject.Create(null, "world");
+
+            Assert.Null(result.Name);
+            Assert.Equal("world", result.Value);
+        }
+
+        [Fact]
+        public void Create_WhenValueIsNull_SetsNullValueInReturnObject()
+        {
+            var result = _subject.Create("hello", null);
+
+            Assert.Equal("hello", result.Name);
+            Assert.Null(result.Value);
+        }
+    }
+}
