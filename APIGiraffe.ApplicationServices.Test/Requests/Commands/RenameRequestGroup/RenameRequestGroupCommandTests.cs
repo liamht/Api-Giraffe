1d372b0df9e7746f47a00c7b623de437619c9435
liamht <ljht93@hotmail.com>
2018-07-19 23:10:30 +0100

    Started to set up renamerequestgroup command with TDD set up

---
+++ APIGiraffe.ApplicationServices.Test/Requests/Commands/RenameRequestGroup/RenameRequestGroupCommandTests.cs
@@ -0,0 +1,21 @@
+﻿
+using APIGiraffe.ApplicationServices.Requests.Commands.RenameRequestGroup;
+using APIGiraffe.Data.UnitOfWork;
+using Moq;
+
+namespace APIGiraffe.ApplicationServices.Test.Requests.Commands.RenameRequestGroup
+{
+    public class RenameRequestGroupCommandTests
+    {
+        private RenameRequestGroupCommand _subject;
+
+        private Mock<IUnitOfWork> _uow;
+
+        public RenameRequestGroupCommandTests()
+        {
+            _uow = new Mock<IUnitOfWork>();
+
+            _subject = new RenameRequestGroupCommand(_uow.Object);
+        }
+    }
+}
---
+++ APIGiraffe.ApplicationServices/Requests/Commands/RenameRequestGroup/RenameRequestGroupCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using APIGiraffe.Data.UnitOfWork;
+
+namespace APIGiraffe.ApplicationServices.Requests.Commands.RenameRequestGroup
+{
+    public class RenameRequestGroupCommand
+    {
+        private IUnitOfWork _uow;
+
+        public RenameRequestGroupCommand(IUnitOfWork uow)
+        {
+            _uow = _uow;
+        }
+
+        public void Execute(int requestGroupId, string newName)
+        {
+
+        }
+    }
+}
