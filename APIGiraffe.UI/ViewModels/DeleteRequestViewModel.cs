65e2e72d4cf08b01d2b5c0baa5dd7169e923cbbf
Liam Hayman Tansley <ljht93@hotmail.com>
2018-08-03 08:40:19 +0100

    ensured menu gets refreshed after deleting a request

--- APIGiraffe.UI/ViewModels/DeleteRequestViewModel.cs
+++ APIGiraffe.UI/ViewModels/DeleteRequestViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using APIGiraffe.ApplicationServices.Requests.Commands.DeleteRequest;
+using APIGiraffe.UI.Navigation;
 using APIGiraffe.UI.ViewModels.Commands;
 using System.Windows.Input;
 
@@ -10,11 +11,15 @@ namespace APIGiraffe.UI.ViewModels
 
         public ICommand ConfirmDeleteCommand { get; private set; }
 
-        public int Id { get; private set; }              
+        public int Id { get; private set; }
 
-        public DeleteRequestViewModel(IDeleteRequestCommand command)
+        public DeleteRequestViewModel(IDeleteRequestCommand command, INavigationHelper navigationHelper)
         {
-            ConfirmDeleteCommand = new ActionCommand(() => command.Execute(Id));
+            ConfirmDeleteCommand = new ActionCommand(() =>
+            {
+                command.Execute(Id);
+                navigationHelper.RefreshMenu();
+            });
         }
 
         public void LoadValues(int id)
