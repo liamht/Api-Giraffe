4cee9edc603fcdab16732d2d2f914901080c2df4
Liam Hayman Tansley <ljht93@hotmail.com>
2018-08-03 08:29:22 +0100

    created delete request dialog

---
+++ APIGiraffe.UI/Views/DeleteRequestDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace APIGiraffe.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for DeleteRequestDialog.xaml
+    /// </summary>
+    public partial class DeleteRequestDialog : UserControl
+    {
+        public RenameRequestDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
