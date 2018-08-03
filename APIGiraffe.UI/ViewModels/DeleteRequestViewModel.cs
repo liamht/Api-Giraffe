2cdb1d2dc73da785d2ddb503599c5c3a251c45bd
Liam Hayman Tansley <ljht93@hotmail.com>
2018-08-03 08:24:22 +0100

    Added delete request view model

---
+++ APIGiraffe.UI/ViewModels/DeleteRequestViewModel.cs
@@ -0,0 +1,23 @@
+﻿using APIGiraffe.ApplicationServices.Requests.Commands.DeleteRequest;
+using APIGiraffe.UI.ViewModels.Commands;
+using System.Windows.Input;
+
+namespace APIGiraffe.UI.ViewModels
+{
+    public class DeleteRequestViewModel
+    {
+        public ICommand ConfirmDeleteCommand { get; private set; }
+
+        public int Id { get; private set; }
+
+        public DeleteRequestViewModel(IDeleteRequestCommand command)
+        {
+            ConfirmDeleteCommand = new ActionCommand(() => command.Execute(Id));
+        }
+
+        public void LoadValues(int id)
+        {
+            Id = id;
+        }
+    }
+}
